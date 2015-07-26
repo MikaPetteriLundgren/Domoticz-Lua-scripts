0ea246ae57436767a5c8170b33b44dfc32fdd922
Mika-Petteri Lundgren <mlundgre@gmail.com>
2015-07-26 20:43:44 +0300

    Email address, subject and body texts changed to variables. Alarm siren and fire alarm are turned off only if there has been a security alarm.

--- script_security_securityalarmcompanion.lua
+++ script_security_securityalarmcompanion.lua
@@ -1,5 +1,5 @@
 --[[Intention of this script is to work as a companion script for the ”script_device_securityalarm.lua” script. 
-	The script triggers if security device is set to Normal state. Script turns alarm siren and fire alarm off when triggered.
+	The script triggers if security device is set to Normal state. Script turns alarm siren and fire alarm off only if there has been a security alarm.
 	Script runs every time state of the security device is changed.
 	Script also sends notification when security device is armed and disarmed.
 	Own variable fireAlarmActivationType (type: Int) is used to determine if fire alarm device is activated by the security alarm script or not. If yes, then value is 1 else 0 (real fire alarm)]]
@@ -9,6 +9,10 @@
 --------------------------------
 alarmActivationType = "fireAlarmActivationType" --Variable which is used to determine has fire alarm been activated by security alarm (1) or by fire alarm (0)
 alarmSiren = "Sireeni" --Name of the alarm siren device
+subjectTextArm = "Domoticz - Security alarm has been switched on" --Subject text of the email/SMS/notification in case of arming the system
+subjectTextDisarm = "Domoticz - Security alarm has been switched off" --Subject text of the email/SMS/notification in case of disarming the system
+bodyTextArm = "Security alarm has been switched on." --Body text of the email/SMS/notification in case of arming the system
+bodyTextDisarm = "Security alarm has been switched off." --Body text of the email/SMS/notification in case of disarming the system
 debug = false
 --------------------------------
 -- End of variables to edit --
@@ -29,11 +33,15 @@ if (globalvariables["Security"] == "Disarmed") then -- If security device is dis
 		print(activationType)
 	end
 	
-	commandArray[alarmSiren] = "Disarm" --Turn alarm siren off
-	print("Security device disarmed...")
+	if (activationType == 1) then
+		commandArray[alarmSiren] = "Disarm" --Turn alarm siren off
+		commandArray["OpenURL"]="http://192.168.1.93:8080/json.htm?type=command&param=resetsecuritystatus&idx=34&switchcmd=Normal" -- Set fire alarm device back to Normal mode from Panic mode
+		commandArray["Variable:"..alarmActivationType] = tostring(0) ----Fire alarm device activation type is set to 0
+		print("Siren and fire alarm turned off")
+	end
 	
-	commandArray["Variable:"..alarmActivationType] = tostring(0) ----Fire alarm device activation type is set to 0
-	commandArray['SendNotification']="Domoticz - Security alarm has been switched off#Security alarm has been switched off.#0"
+	print("Security device disarmed...")
+	commandArray['SendNotification']=subjectTextDisarm.."#"..bodyTextDisarm.."#0"
 	
 elseif (globalvariables["Security"] ~= "Disarmed") then -- If security device is armed, send notification
 	print("Security alarm companion script running...")
@@ -44,7 +52,7 @@ elseif (globalvariables["Security"] ~= "Disarmed") then -- If security device is
 	end
 	 
 	print("Security device armed!!!")
-	commandArray['SendNotification']="Domoticz - Security alarm has been switched on#Security alarm has been switched on.#0"
+	commandArray['SendNotification']=subjectTextArm.."#"..bodyTextArm.."#0"
 
 
 end
