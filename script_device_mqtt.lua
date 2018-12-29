701e4a06e35175084ccd08d47ae3b360ae632c0b
Mika-Petteri Lundgren <mlundgre@gmail.com>
2018-12-29 18:11:46 +0200

    Script rewritten

--- script_device_mqtt.lua
+++ script_device_mqtt.lua
@@ -1,25 +1,13 @@
---[[Intention of this script is to send devicechanges of a pre-defined device to localhost port 5001 if 
-	there is any value to publish via MQTT protocol. A node.js script is running on same Raspberry Pi and publishes message via MQTT
-	This script is mainly based on a script which was found from Domoticz MQTT wiki entry
-
- 
-var url = require("url");
-var http = require("http");
-var pubclient = new mqtt.MQTTClient(1883, "127.0.0.1", "domoticz");
- 
-http.createServer(function (req, res) {
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.end("Response from Node.js \n");
-	pubclient.publish("/events/domoticz"+url.parse(req.url).pathname, url.parse(req.url).query);	
-}).listen(5001, "127.0.0.1");
- 
-NB. Domoticz will add _ and property to the name of certain sensors (e.g. _temperature, _humidity). This is passed as lowest level of message in mqtt
+--[[
+Intention of this script is to publish defined message using MQTT broker running on a localhost.
+The script will trigger only when the status of the defined device is changed.
 --]]
 
 --------------------------------
 ------ Variables to edit ------
 --------------------------------
-devicename = "Sireeni" --Name of the device
+devicename = "Hinnerjoki - Sireeni" --Name of the device
+deviceIDX = 22
 debug = false
 --------------------------------
 -- End of variables to edit --
@@ -28,22 +16,15 @@ debug = false
 commandArray = {}
  
 if (devicechanged[devicename]) then
-	print(devicename.." MQTT script running...") 
-
-	--d = otherdevices
-	device = ""
-	for i, v in pairs(devicechanged) do
-	  if (#device == 0 or #i < #device) then device = i end
-	  text = v..""
-	  if #text > 0 then
-		text = string.gsub(i, "_", "/")
-		text = "127.0.0.1:5001/"..text.."?"..v
-		text = string.gsub(text, "%s+", "%%20")
-		commandArray["OpenURL"]=text
-		if (debug) then print(text) end
-	  end
+	print(devicename.." MQTT script running...")
+	
+	if (devicechanged[devicename] == "On") then
+		print(devicename.." turned on!")
+		os.execute('mosquitto_pub -h 127.0.0.1 -t domoticz/out -m '..deviceIDX..":1")
+	elseif (devicechanged[devicename] == "Off") then
+		print(devicename.." turned off!")
+		os.execute('mosquitto_pub -h 127.0.0.1 -t domoticz/out -m '..deviceIDX..":0")
 	end
-
 end
  
 return commandArray
